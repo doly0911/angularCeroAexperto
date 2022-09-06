6b9de2eedeffc2d9aedfd65af9a00e9881bdbf54
Doly Jimenez <doly.jimenez11@gmail.com>
2022-09-05 21:43:38 -0500

    add HTTP client module for showing images in resultados component

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = 'Z1JlknMecLdmnbCbbTAklkdVKKi9PxcE'
   private _historial: string[] = []
+  public resultados: Gif[] = []
 
   get historial() {
     return [...this._historial]
   }
+
+  constructor(private http: HttpClient){}
 
   buscarGift(query: string){
 
@@ -24,9 +30,19 @@ export class GifsService {
       this._historial = this._historial.slice(0,10)
     }
 
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=Z1JlknMecLdmnbCbbTAklkdVKKi9PxcE&q=${query}&limit=10`)
+    .subscribe(resp => {
+      this.resultados = resp.data
+      console.log("data compelta:", resp);
 
+      console.log("resultados del Gif: ", this.resultados);
 
-    console.log("array de gifs", this._historial)
+    })
   }
+
+
+
+
+
 
 }
