e48cf2fd69e446596fc5cc249d7b09b05d5d96f3
Doly Jimenez <doly.jimenez11@gmail.com>
2022-09-14 19:35:50 -0500

    basic example about primeNg

---
+++ 04-pipesApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//primeNg
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ButtonModule,
+    CardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
