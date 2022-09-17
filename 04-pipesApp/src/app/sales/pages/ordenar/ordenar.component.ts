ff91ef62ab545356b0b5e2df76f7b9e25a29716c
Doly Jimenez <doly.jimenez11@gmail.com>
2022-09-17 00:11:58 -0500

    add vuelaPipe, table and toolbar primeNg

--- 04-pipesApp/src/app/sales/pages/ordenar/ordenar.component.ts
+++ 04-pipesApp/src/app/sales/pages/ordenar/ordenar.component.ts
@@ -1,10 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroe.interfaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html'
 })
 export class OrdenarComponent implements OnInit {
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'thor',
+      vuela: false,
+      color: 'rojo'
+    },
+    {
+      nombre: 'falcon',
+      vuela: true,
+      color: 'azul'
+    },
+    {
+      nombre: 'spiderman',
+      vuela: false,
+      color: 'rojo oscuro'
+    },
+
+  ]
 
   constructor() { }
 
--- 04-pipesApp/src/app/sales/sales.module.ts
+++ 04-pipesApp/src/app/sales/sales.module.ts
@@ -6,16 +6,21 @@ import { BasicosComponent } from './pages/basicos/basicos.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
+import { VuelaPipe } from './pipes/vuela.pipe';
 
 
 
 
 @NgModule({
   declarations: [
+    //Componentes
     BasicosComponent,
     NoComunesComponent,
     NumerosComponent,
-    OrdenarComponent
+    OrdenarComponent,
+
+    //Pipes
+    VuelaPipe
   ],
   imports: [
     CommonModule,
